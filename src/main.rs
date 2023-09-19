0ebf04802d70ddefa633992fe9e17cf5296fcf21
JockeRider199 <jockerider199@gmail.com>
2023-09-19 22:31:14 +0200

    feat: display metadata on file

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,7 @@ use dialoguer::theme::ColorfulTheme;
 use dialoguer::{Input, Select};
 use home::home_dir;
 use std::path::Path;
-use std::thread;
-use std::{
-    process::{self, Command},
-    time::Duration,
-};
+use std::process::{self, Command};
 
 fn main() -> Result<()> {
     let choices = vec!["Video", "Audio", "Get versions", "Exit"];
@@ -39,20 +35,23 @@ fn audio() -> Result<()> {
 
     let default_location = Path::new(&home_dir().unwrap()).join("Downloads");
     let location: String = Input::new()
-        .with_prompt("Enter the location where you want to save the video")
+        .with_prompt("Enter the location where you want to save the file")
         .default(default_location.to_str().unwrap().to_string())
         .interact_text()?;
 
     let mut command = Command::new("yt-dlp");
 
     command
-        .current_dir(location)
+        .current_dir(&location)
         .arg("-x")
         .arg("--audio-format")
         .arg("mp3")
+        .arg("-O")
+        .arg("id,title,filesize,filesize_approx,duration_string")
+        .arg("--no-simulate")
         .arg(url);
 
-    download(&mut command);
+    download(&mut command, &location, None);
 
     Ok(())
 }
@@ -81,34 +80,51 @@ fn video() -> Result<()> {
     let mut command = Command::new("yt-dlp");
 
     command
-        .current_dir(location)
-        .arg("-i")
+        .current_dir(&location)
         .arg("-f")
         .arg(format)
+        .arg("-O")
+        .arg("id,title,filesize,filesize_approx,duration_string")
+        .arg("--no-simulate")
         .arg(url);
 
-    download(&mut command);
+    download(&mut command, &location, Some(choices[quality]));
 
     Ok(())
 }
 
-fn download(command: &mut Command) {
+fn download(command: &mut Command, location: &str, quality: Option<&str>) {
     let output = command.output();
 
     match output {
-        Ok(_) => println!(
-            "\n {}",
-            console::style("Downloaded successfully").green().bold()
-        ),
+        Ok(output) => {
+            let out = String::from_utf8_lossy(&output.stdout);
+            let informations: Vec<&str> = out.split("\n").collect();
+            println!(
+                "\n{}\n\n{}\nYoutube ID: {}\nName: '{}'\nFile size: {}MB\nLocation: {}\n{}Length: {}\n",
+                console::style("Downloaded successfully").green().bold(),
+                console::style("Informations").bold(),
+                informations[0],
+                informations[1],
+                format!(
+                    "{:.1$}",
+                    informations[2].parse::<f64>().unwrap_or(informations[3].parse::<f64>().unwrap_or(0.0)) / 1000000.0,
+                    1
+                ),
+                location,
+                match quality {
+                    Some(quality) => format!("Definition: {}\n", quality),
+                    None => String::new(),
+                },
+                informations[4]
+            )
+        }
         Err(e) => println!(
             "\n {}\n{}",
             console::style("Failed to download").red().bold(),
             e
         ),
     }
-
-    thread::sleep(Duration::from_millis(2000));
-    Command::new("clear").status().unwrap();
 }
 
 fn get_version() -> String {
